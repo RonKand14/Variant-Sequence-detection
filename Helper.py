9e63a0a3dfc2c2e5dbbd8bb6a200f6e49f14bb22
Kenny-Powers <57463444+RonKand14@users.noreply.github.com>
2023-08-12 20:29:50 +0300

    Add files via upload

---
+++ Helper.py
@@ -0,0 +1,109 @@
+from Bio import SeqIO
+import pandas as pd
+import numpy as np
+from keras.optimizers import SGD
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Flatten, Conv1D, MaxPooling1D
+from keras.layers import BatchNormalization
+from keras import initializers
+
+
+class Helpers:
+    
+    def __init__(self, csv_path, fasta_path):
+        self.csv_path = csv_path
+        self.fasta_path = fasta_path
+        self.linages = None
+        self.mapping_dict = dict()
+        self.genome_to_linage = dict()
+        self.names = None
+        self.seq = None
+        self.model = None
+        self.labels = []
+        self.epochs = None
+
+    def parseFasta(self):
+        seq_list = []
+        name_list = []
+        fasta_sequences = SeqIO.parse(open(self.fasta_path), 'fasta')
+        index = 0
+        for fasta in fasta_sequences:
+            name, sequence = fasta.id, str(fasta.seq)
+            print(f"index {index}")
+            index = index+1
+            splited_name = name.split("|")[1]
+            name_list.append(splited_name)
+            seq_list.append(sequence)
+            if index == 10000:
+                break
+        print("Done fasta ")
+        self.names = name_list
+        self.seq = seq_list
+        return name_list, seq_list
+
+    def parseAssToLinage(self):
+        df = pd.read_csv(self.csv_path)
+        acc_id = df["accession_id"]
+        linage = df["lineage"]
+        linage.drop_duplicates(inplace=True)
+        self.linages = linage
+        for value in linage:
+            self.mapping_dict[value] = []
+            self.genome_to_linage[value] = []
+        for row in df.iterrows():
+            id = row[1]["accession_id"]
+            lin = row[1]["lineage"]
+            self.mapping_dict[lin].append(id)
+
+    def mapGenomeToLinage(self):
+        for index in range(0, len(self.names)):
+            name = self.names[index]
+            genome = self.seq[index]
+            for key in self.mapping_dict.keys():
+                if name in self.mapping_dict[key]:
+                    self.genome_to_linage[key].append(genome)
+
+    def oneHot(self, seq):
+        seq2 = list()
+        #mapping = {"A": [1., 0., 0., 0.], "C": [0., 1., 0., 0.], "G": [0., 0., 1., 0.], "T": [0., 0., 0., 1.]}
+        mapping = {"A": 2, "C": 3, "G": 4, "T": 5}
+        for i in seq:
+            seq2.append(mapping[i] if i in mapping.keys() else 1)
+        return np.array(seq2)
+
+    def mapGenomeToVector(self):
+        features = []
+        for se in self.seq:
+            features.append(self.oneHot(se))
+            self.getGenomeKey(se)
+        return features
+
+    def getGenomeKey(self, seq):
+        for key in self.genome_to_linage:
+            if seq in self.genome_to_linage[key]:
+                self.labels.append(key)
+                break
+
+    def initModel(self, train_features, n_output):
+        model = Sequential()
+        model.add(Conv1D(filters=256, kernel_size=7, input_shape=(train_features.shape[1], 1),
+                         padding='same', activation='relu'))
+        model.add(MaxPooling1D(pool_size=2))
+        model.add(Dropout(0.5))
+        model.add(Conv1D(filters=24, kernel_size=12, input_shape=(train_features.shape[1], 1),
+                         padding='same', activation='relu'))
+        # model.add(MaxPooling1D(pool_size=2))
+        model.add(MaxPooling1D(pool_size=2))
+        model.add(BatchNormalization())
+        model.add(Flatten())
+        #model.add(Dense(n_output, activation='sigmoid', kernel_initializer=initializers.RandomNormal(stddev=0.01),bias_initializer=initializers.Zeros()))
+        # model.add(Dropout(0.2))
+        #model.add(Dense(n_output, activation='softmax'))
+        model.add(Dense(n_output, activation='sigmoid', kernel_initializer=initializers.RandomNormal(stddev=0.01),bias_initializer=initializers.Zeros()))
+        lrate = 0.1
+        # decay = lrate / epochs
+        decay = 0.0025
+        sgd = SGD(learning_rate=lrate, momentum=0.90, decay=decay, nesterov=False)
+        model.compile(loss='categorical_crossentropy', optimizer="adam", metrics=['accuracy'])
+        model.summary()
+        self.model = model
---
+++ main.py
@@ -0,0 +1,90 @@
+from Helper import Helpers
+from sklearn.preprocessing import OneHotEncoder
+import tensorflow as tf
+import numpy as np
+# from tensorflow.keras.models import load_model
+from sklearn.model_selection import train_test_split
+from sklearn import metrics
+from keras.models import load_model
+from keras.callbacks import ModelCheckpoint
+import pandas as pd
+import csv
+
+
+# Data Path
+print("Step 1: Data path init")
+input_file = "./data/final/ena_data_20230624-2230.fasta"
+csv_path = "./data/final/covid19data_embl-covid19.tsv.csv"
+output_file = "./data/output.txt"
+load = True     # load from file
+continue_fit = True
+# Helper class Init
+print("Helper class init")
+helper = Helpers(csv_path, input_file)
+
+# Data Parsing (ID to Lineage + Genome to Lineage) - All values will be stores in dicts
+print("Data Parsing (ID to Lineage + Genome to Lineage) - All values will be stores in dicts")
+if not load:
+    helper.parseAssToLinage()
+    names, seq = helper.parseFasta()
+    helper.mapGenomeToLinage()
+
+    # Data decoder (Genome as Vectors instead of strings)
+    print("Data decoder (Genome as Vectors instead of strings)")
+    gen_features = helper.mapGenomeToVector()
+    label = helper.labels
+    labels = label
+    for i in range(0,19):
+        label = np.append(label, labels[0])
+    padding = tf.keras.preprocessing.sequence.pad_sequences(gen_features, padding="post")
+    input_features = np.stack(padding)
+
+    one_hot_encoder = OneHotEncoder()
+    label = np.array(label).reshape(-1, 1)
+    input_labels = one_hot_encoder.fit_transform(label).toarray()
+else:
+    input_features = np.memmap('data.bin', mode='r+', dtype=int, shape=(25000, 31190))
+    input_features = np.array(input_features)
+    input_labels = np.memmap('label_2.bin', mode='r+', dtype=int, shape=(25000, 369))
+    input_labels = np.array(input_labels)
+
+# Data Split (Train, Validation and test)
+train_features, test_features, train_labels, test_labels = train_test_split(
+    input_features, input_labels, test_size=0.20, random_state=42)
+
+# Model Init
+print("Model")
+n_output = input_labels.shape[1]
+np.expand_dims(train_features, 0)
+batch_size = 18
+val_split = 0.3
+epochs = 1
+
+# Model Training
+filepath_read = "./model/model_1.h5"
+if continue_fit:
+    # fit the new model
+    new_model = load_model(filepath_read)
+    checkpoint = ModelCheckpoint(filepath_read, monitor='loss', verbose=1, save_best_only=True, mode='min')
+    callbacks_list = [checkpoint]
+    new_model.fit(train_features, train_labels, verbose=1, epochs=epochs, batch_size=batch_size, callbacks=callbacks_list, validation_split=val_split)
+    helper.model = new_model
+else:
+    helper.initModel(train_features, n_output)
+
+# Model Training
+filepath = "./model/model_2.h5"
+# checkpoint
+checkpoint = ModelCheckpoint(filepath, monitor='loss', verbose=1, save_best_only=True, mode='min')
+callbacks_list = [checkpoint]
+
+# Model testing
+x = helper.model.evaluate(test_features, test_labels)
+predicted_labels = helper.model.predict(np.stack(test_features))
+confusion = metrics.confusion_matrix(np.argmax(test_labels, axis=1), np.argmax(predicted_labels, axis=1))
+TP = confusion[1, 1]
+FP = confusion[0, 1]
+TN = confusion[0, 0]
+FN = confusion[1, 0]
+scores = helper.model.evaluate(test_features, test_labels, verbose=0)
+print("Accuracy: %.2f%%" % (scores[1]*100))
